0765bdd986fbdc4ed121493a7975b33f3ec436fd
Petr Vasilev <vahpetr@gmail.com>
2024-03-16 22:54:16 +0300

    Add ya metrics

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Game Design Concepts
 
-This project tests my other website scraper project Scrapy Dogs. Scrapy Dogs is app for convert html page to md/docfx/pdf/epub books formats. For testing, I chose [aushestov.ru](http://aushestov.ru/шрайбер/).
+This project tests my other website scraper project Scrapy Dogs. Scrapy Dogs is app for convert html page to md/docfx/pdf/epub books formats. For testing, I chose [aushestov.ru/шрайбер](http://aushestov.ru/шрайбер/).
 
 ## How to use locally
 
--- src/docs/template/public/main.js
+++ src/docs/template/public/main.js
@@ -1,6 +1,35 @@
 export default {
   lunrLanguages: ["en", "ru"],
   start: () => {
+    const YAID = 96761155;
+
+    (function (m, e, t, r, i, k, a) {
+      m[i] =
+        m[i] ||
+        function () {
+          (m[i].a = m[i].a || []).push(arguments);
+        };
+      m[i].l = 1 * new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) {
+          return;
+        }
+      }
+      (k = e.createElement(t)),
+        (a = e.getElementsByTagName(t)[0]),
+        (k.async = 1),
+        (k.src = r),
+        a.parentNode.insertBefore(k, a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(YAID, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      trackHash: true,
+      webvisor: true,
+    });
+
     if (!window.speechSynthesis) {
       console.log("SpeechSynthesis is not supported in this browser.");
       return;
@@ -55,6 +84,7 @@ export default {
       }
 
       speakButton.onclick = (ev) => {
+        ym(YAID, "reachGoal", "use_speech_synthesis");
         if (!speaking) {
           if (sentences.length === 0 || currentSentenceIndex === 0) {
             const articleText =
